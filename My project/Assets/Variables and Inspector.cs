9592763157d85bd36efa22044aba14f0e632abfd
Katherine Brower <ktbrower101@gmail.com>
2022-10-06 20:30:58 -0600

    Variables and Inspector

---
+++ My project/Assets/Variables and Inspector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesandInspector : MonoBehaviour
+{
+    public string myName;
+    void Start()
+    {
+        Debug.Log("I am alive and my name is" + myName);
+    }
+
+    void Update()
+    {
+        
+    }
+}
